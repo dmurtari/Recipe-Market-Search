cc242ee7c3e877245cccca30e7f8cf3a57ef537f
Domenic Murtari <domenic.murtari@gmail.com>
2015-03-06 22:48:04 -0700

    Restored markets

---
+++ contents/index.md
@@ -0,0 +1,4 @@
+---
+title: Recipes and Markets
+template: layout.jade
+---
---
+++ contents/recipes/index.js
@@ -0,0 +1,30 @@
+var recipes = {
+
+    searchByIngredient: function(ingredient) {
+
+        $.get("http://food2fork-api.herokuapp.com/recipe/search/" + ingredient, function(data) {
+
+            $.get("/recipesearch/recipes/list.jade", function(template) {
+                var html = jade.render(template, {
+                    data: data
+                })
+                $("#list").html(html)
+            })
+
+        })
+
+    },    
+
+    load: function() {
+
+        $.get("/recipesearch/recipes/ui.jade", function(template) {
+            var html = jade.render(template)
+            $("#ui").html(html)
+        })
+
+        // default search results
+        recipes.searchByChamber('Chicken')
+
+    }
+
+}
